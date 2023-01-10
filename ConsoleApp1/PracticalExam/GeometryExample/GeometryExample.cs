93e8d54304ee3f00e38215a10e8046a98cdc8a0d
QTruonggg <quangtruong102000@gmail.com>
2023-01-10 19:20:24 +0700

    ex

--- ConsoleApp1/PracticalExam/GeometryExample/GeometryExample.cs
+++ ConsoleApp1/PracticalExam/GeometryExample/GeometryExample.cs
@@ -8,6 +8,13 @@ namespace ConsoleApp1.PracticalExam.GeometryExample
 {
     public class GeometryExample
     {
-        
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder();
+            cylinder.Input();
+            cylinder.Process(cylinder.Radius, cylinder.Height);
+            Console.WriteLine("Cylinder Characteristics: ");
+            Console.WriteLine(cylinder.Result());
+        }
     }
 }
