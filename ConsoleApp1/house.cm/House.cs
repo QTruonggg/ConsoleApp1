3eb448a4daefb17bc618755da2456ed7bae86502
QTruonggg <quangtruong102000@gmail.com>
2023-01-10 18:32:40 +0700

    PracticalExam

---
+++ ConsoleApp1/house.cm/House.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2203E_CSharp.house.cm
+{
+    class House : IHouse
+    {
+        private string soNha;
+        private string diaChi;
+        private string loaiNha;
+
+        public House()
+        {
+            soNha = "";
+            diaChi = "";
+            loaiNha = "";
+        }
+
+        public House(string soNha, string diaChi, string loaiNha)
+        {
+            this.soNha = soNha;
+            this.diaChi = diaChi;
+            this.loaiNha = loaiNha;
+        }
+        public string SoNha
+        {
+            get { return soNha; }
+            set { soNha = value; }
+        }
+        public string DiaChi
+        {
+            get { return diaChi; }
+            set { diaChi = value; }
+        }
+        public string LoaiNha
+        {
+            get { return loaiNha; }
+            set { loaiNha = value; }
+        }
+        public void input()
+        {
+            Console.WriteLine("Nhập số nhà: ");
+            soNha = Console.ReadLine();
+            Console.WriteLine("Nhập địa chỉ: ");
+            diaChi = Console.ReadLine();
+            Console.WriteLine("Nhập loại nhà: ");
+            loaiNha = Console.ReadLine();
+        }
+        public void display()
+        {
+            Console.WriteLine("Số nhà: " + soNha);
+            Console.WriteLine("Địa chỉ: " + diaChi);
+            Console.WriteLine("Loại nhà: " + loaiNha);
+        }
+
+    }
+}
---
+++ ConsoleApp1/house.cm/IHouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2203E_CSharp.house.cm
+{
+    public interface IHouse
+    {
+        public void input();
+        public void display();
+    }
+}
